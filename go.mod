4b867e413a714b26c3e7af8c2076754891693140
John Masters <mastersjohnr@gmail.com>
2024-05-12 21:54:36 +1000

    feat: html files > templ files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module autojob
 
 go 1.22.3
+
+require github.com/a-h/templ v0.2.680 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"autojob/components"
 	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "templates/index.html")
+		component := components.Home()
+
+		component.Render(r.Context(), w)
 	})
 
 	http.HandleFunc("/sign-up", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "templates/sign-up.html")
+		component := components.Signup()
+
+		component.Render(r.Context(), w)
 	})
 
 	fmt.Println("Server running on http://localhost:8080")
