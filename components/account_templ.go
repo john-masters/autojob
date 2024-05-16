14418d6b2d0687eac2ec259e1714810df4b7f28c
John Masters <mastersjohnr@gmail.com>
2024-05-16 20:41:04 +1000

    feat: create experience form

--- components/account_templ.go
+++ components/account_templ.go
@@ -31,6 +31,10 @@ func Account(user models.User) templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
+			templ_7745c5c3_Err = Experience().Render(ctx, templ_7745c5c3_Buffer)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
 			if !templ_7745c5c3_IsBuffer {
 				_, templ_7745c5c3_Err = io.Copy(templ_7745c5c3_W, templ_7745c5c3_Buffer)
 			}
