600f99b1d8f4ff50453f7e0aa576ef42e4149f21
John Masters <mastersjohnr@gmail.com>
2024-05-26 17:17:26 +1000

    feat: add ai integration

---
+++ utils/ai.go
@@ -0,0 +1,83 @@
+package utils
+
+import (
+	"autojob/models"
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+)
+
+func askGPT(message string) (string, error) {
+	url := "https://api.openai.com/v1/chat/completions"
+
+	systemPrompt := `You are a job matching assistant.
+	Your task is to evaluate whether the user cover letter and user job history match the requirements of a job description.
+	You will receive a JSON payload with a user cover letter, user job history, job title, and job description.
+	Based on this information, you will determine if there is a match (isMatch: true or false).
+	If isMatch is true, you will also provide a custom cover letter tailored to the job description.
+
+	The response should be in JSON format with the following structure:
+	{
+		"isMatch": boolean,
+		"coverLetter": string
+	}
+
+	If isMatch is false, the coverLetter should be an empty string.
+
+	Consider the following when making your decision:
+	- Relevance of job history to the job description
+	- Skills and experiences mentioned
+	- Any other relevant information
+
+	Make sure the cover letter is professional, concise, and highlights the candidate's strengths in relation to the job description.`
+
+	requestBody := models.ChatCompletionRequest{
+		Model: "gpt-3.5-turbo",
+		ResponseFormat: map[string]string{
+			"type": "json_object",
+		},
+		Messages: []models.Message{
+			{Role: "system", Content: systemPrompt},
+			{Role: "user", Content: message},
+		},
+	}
+
+	jsonData, err := json.Marshal(requestBody)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("OPENAI_API_KEY"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", err
+	}
+
+	var response models.ChatCompletionResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return "", err
+	}
+
+	return response.Choices[0].Message.Content, nil
+}
--- utils/user.go
+++ utils/user.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"autojob/db"
 	"autojob/models"
+	"encoding/json"
 	"log"
 )
 
@@ -17,6 +18,59 @@ func processUser(user models.User) {
 	for _, query := range queriesList {
 		scrapeJobData(&jobs, query.Query)
 	}
-	log.Println(jobs)
 
+	var jobHistory []models.History
+	err = db.SelectHistoriesByUserID(user.ID, &jobHistory)
+	if err != nil {
+		log.Fatal("Error getting job history: ", err)
+	}
+
+	var coverLetter models.Letter
+
+	err = db.SelectLetterByUserID(user.ID, &coverLetter)
+	if err != nil {
+		log.Fatal("Error getting cover letter: ", err)
+	}
+
+	jsonHistory, err := json.Marshal(jobHistory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var applyList []models.Apply
+
+	for _, job := range jobs {
+		data := models.UserPromptData{
+			CoverLetterContent: coverLetter.Content,
+			JobHistory:         string(jsonHistory),
+			JobTitle:           job.Title,
+			JobDescription:     job.Description,
+		}
+
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		jsonResponse, err := askGPT(string(jsonData))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var response models.Response
+
+		err = json.Unmarshal([]byte(jsonResponse), &response)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if response.IsMatch {
+			applyList = append(applyList, models.Apply{
+				Title:       job.Title,
+				Company:     job.Company,
+				Link:        job.Link,
+				Description: job.Description,
+				CoverLetter: response.CoverLetter,
+			})
+		}
+	}
 }
